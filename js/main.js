8530f98a2d2e4e740923791671e5e8e2a9cd7818
jaxry <cabot696@gmail.com>
2014-08-07 14:52:22 -0600

    Initial Commit
    Trying to figure out why the framebuffers fail to create on some
    computers.

--- js/main.js
+++ js/main.js
@@ -391,8 +391,8 @@ function makeFbo(gl, width, height, params) {
         gl.bindTexture(gl.TEXTURE_2D, texture);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
 
         var format = params && params.format ? params.format : gl.RGB;
         var type = params && params.type ? params.type : gl.UNSIGNED_BYTE;
